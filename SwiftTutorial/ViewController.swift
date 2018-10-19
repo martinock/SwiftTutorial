ee28fd7d810dec4b333914b17cb97ab60a890983
Nakama <nakama@201546-iMac.local>
2018-10-19 09:35:32 +0700

    Create simple view and action

--- SwiftTutorial/ViewController.swift
+++ SwiftTutorial/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var signButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+//        titleLabel.text = "Success"
     }
 
+    @IBAction func onLogin(_ sender: Any) {
+        if let username = usernameTextField.text, let password = passwordTextField.text {
+            if username != "", password != "" {
+                titleLabel.text = "Hello, " + username + "!"
+            } else {
+                titleLabel.text = "Hello, anonymous!"
+            }
+            
+        } else {
+            titleLabel.text = "Hello, anonymous!"
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
